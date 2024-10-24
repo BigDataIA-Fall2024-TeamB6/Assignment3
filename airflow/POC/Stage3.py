8a9cc18b1881081c9b824600b4ea4dc0856c7a44
solankiram2023 <145072212+solankiram2023@users.noreply.github.com>
2024-10-24 11:16:21 -0400

    upload folder to s3 bucket

--- airflow/POC/Stage3.py
+++ airflow/POC/Stage3.py
@@ -2,7 +2,8 @@ import boto3
 from botocore.exceptions import NoCredentialsError, ClientError 
 
 def upload_to_s3(local_file, bucket, s3_file):
-    s3 = boto3.client('s3')
+    #s3 = boto3.client('s3')
+    s3 = boto3.client('s3', aws_access_key_id='AKIAU6GD3H5T44SFJJV2', aws_secret_access_key='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')
     try:
         s3.upload_file(local_file, bucket, s3_file)
         print (f"Upload Successful: {local_file} to s3://{bucket}/{s3_file}")
@@ -17,7 +18,7 @@ def upload_to_s3(local_file, bucket, s3_file):
         print("Client error: {e}")
         return False
     
-    local_file = "cover_image.jpg"
+    local_file = "/Users/ramyyogeshkumarsolanki/Documents/GitHub/Assignment3/airflow/POC/Stage3.py"
     bucket = "ddddemo-damg7245-fall2024"
     s3_file = "cover_image.jpg"
     upload_to_s3(local_file, bucket, s3_file)
